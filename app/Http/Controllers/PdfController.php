4c74ef786eaa98528e3c07b1046135e6889bac83
NickHuijgen <nick.huijgen@student.hu.nl>
2021-09-28 15:48:30 +0200

    Added queue function to mail

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendPdfMail;
 use App\Mail\QuotationMade;
 use App\Models\Quotation;
 use App\Models\User;
@@ -10,33 +11,38 @@ use Illuminate\Http\Request;
 use Dompdf\Dompdf;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
 
 class PdfController extends Controller
 {
     public function makepdf(Quotation $quotation, User $user)
     {
 
-// instantiate and use the dompdf class
-        $dompdf = new Dompdf();
-        $dompdf->loadHtml(view('pdf.quotation_pdf', ['quotation' => $quotation, 'user' => $user]));
+//// instantiate and use the dompdf class
+//        $dompdf = new Dompdf();
+//        $dompdf->loadHtml(view('pdf.quotation_pdf', ['quotation' => $quotation, 'user' => $user]));
+//
+//// (Optional) Setup the paper size and orientation
+//        $dompdf->setPaper('A4', 'portrait');
+//
+//// Render the HTML as PDF
+//        $dompdf->render();
+//
+//// Output the generated PDF to Browser
+//        //Set attachment to false so it doesn't try to download the file when displaying it in browser
+//        $dompdf->stream('quotation.pdf', ['Attachment' => false]);
 
-// (Optional) Setup the paper size and orientation
-        $dompdf->setPaper('A4', 'portrait');
-
-// Render the HTML as PDF
-        $dompdf->render();
-
-// Output the generated PDF to Browser
-        //Set attachment to false so it doesn't try to download the file when displaying it in browser
-        $dompdf->stream('quotation.pdf', ['Attachment' => false]);
+//        Storage::put('public/pdf/quotation.pdf', $dompdf->output());
+//
+//        SendPdfMail::dispatch($quotation);
 
         //Define the new message and add a $quotation
-        $message = new QuotationMade($quotation);
+//        $message = new QuotationMade($quotation, $user);
 
         //Attach the newly created pdf to the $message and name it 'quotation.pdf'
-        $message->attachData($dompdf->output(), "quotation.pdf");
+//        $message->attachData($dompdf->output(), "quotation.pdf");
 
         //Send a mail to a user with the $message attached
-        Mail::to($quotation->customer_email)->queue($message);
+        Mail::to($quotation->customer_email)->queue(new QuotationMade($quotation, $user));
     }
 }
--- app/Mail/QuotationMade.php
+++ app/Mail/QuotationMade.php
@@ -4,6 +4,7 @@ namespace App\Mail;
 
 use App\Models\Quotation;
 use App\Models\User;
+use Dompdf\Dompdf;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -14,6 +15,7 @@ class QuotationMade extends Mailable
     use Queueable, SerializesModels;
 
     public $quotation;
+    public $user;
 
     /**
      * Create a new message instance.
@@ -21,9 +23,11 @@ class QuotationMade extends Mailable
      * @return void
      */
     //Make the quotation variable usable in the mail class
-    public function __construct(Quotation $quotation)
+    public function __construct(Quotation $quotation, User $user)
     {
         $this->quotation = $quotation;
+        $this->user = $user;
+
     }
 
     /**
@@ -33,7 +37,22 @@ class QuotationMade extends Mailable
      */
     public function build()
     {
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml(view('pdf.quotation_pdf', ['quotation' => $this->quotation, 'user' => $this->user]));
+
+// (Optional) Set up the paper size and orientation
+        $dompdf->setPaper('A4');
+
+// Render the HTML as PDF
+        $dompdf->render();
+
+// Output the generated PDF to Browser
+        //Set attachment to false, so it doesn't try to download the file when displaying it in browser
+        $dompdf->stream('quotation.pdf', ['Attachment' => true]);
+
         return $this->markdown('emails.quotation_made')
+            ->attachData($dompdf->output(), "quotation.pdf")
             ->subject('Your quotation');
+
     }
 }
